771c184ed6a5886803564c36dc722a84a1826e12
Raman Karseka <ier@live.ru>
2021-03-15 23:50:02 +0300

    feat: solution for the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-
-exports.min = function min (array) {
-  return 0;
+function compareNumeric(a, b) {
+    if (a > b) return 1;
+    if (a == b) return 0;
+    if (a < b) return -1;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.min = function min(array) {
+    if (arguments.length > 0 && array.length > 0) {
+        array.sort(compareNumeric);
+        return array[0];
+    } else return 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.max = function max(array) {
+    if (arguments.length > 0 && array.length > 0) {
+        array.sort(compareNumeric);
+        return array[(array.length - 1)];
+    } else return 0;
+}
+
+exports.avg = function avg(array) {
+    if (arguments.length > 0 && array.length > 0) {
+        let result = array.reduce((sum, current) => sum + current, 0) / array.length;
+        return result;
+    } else return 0;
 }
